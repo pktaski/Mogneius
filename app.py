19c3135c0f9c0d41e8f47fc44986003f5b453f85
pktaski <142419489+pktaski@users.noreply.github.com>
2023-08-19 20:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://pktaski: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@github.com/pktaski/Filters-Bot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
